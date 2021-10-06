2ee12e3a87947b9e4d648872117ad9981fc3eac3
an aspirin <76911308+JBYT27@users.noreply.github.com>
2021-10-06 12:13:53 +0000

    Updates

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from flask import Flask, render_template
+import json, requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template(
+    'index.html',
+    )
 
 
+app.run(host="0.0.0.0", port=8080)
