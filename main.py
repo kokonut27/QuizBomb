78d853a4eda6d5931dbf2e14d8866eca857303fc
an aspirin <76911308+JBYT27@users.noreply.github.com>
2021-10-29 18:22:39 +0000

    Updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, session, url_for, send_from_directory, redirect
 import json, requests
 
 app = Flask(__name__)
@@ -8,6 +8,17 @@ def index():
   return render_template(
     'index.html',
     )
+
+@app.route('/login')
+def login():
+  return render_template("login.html")
+
+@app.route('/login', methods = ["GET", "POST"])
+def loginrequest():
+  if request.method == "POST":
+    yn = request.form["playnow"]
+  
+  return redirect(url_for('login'))
 
 
 app.run(host="0.0.0.0", port=8080)
