fac92c617f0ef1a6115b857af9ca9f06bb3b37e1
an aspirin <76911308+JBYT27@users.noreply.github.com>
2021-11-15 16:58:20 +0000

    Updates

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, request, flash, session, url_for, send_from_directory, redirect
-import json, requests
+import json
+import requests
+import sqlite3 as sql
 
 app = Flask(__name__)
+
+def add_data_userpass(username, password):  
+  try:
+    # Connecting to database
+    con = sql.connect('userpass.db')
+    # Getting cursor
+    c =  con.cursor() 
+    # Adding data
+    c.execute("INSERT INTO Shots (username, password) VALUES (%s, %s)" %(username, password))
+    # Applying changes
+    con.commit() 
+  except:
+    print("An error has occured!")
 
 @app.route('/')
 def index():
@@ -16,9 +31,29 @@ def login():
 @app.route('/login', methods = ["GET", "POST"])
 def loginrequest():
   if request.method == "POST":
-    yn = request.form["playnow"]
-  
-  return redirect(url_for('login'))
+    username = request.form["username"]
+    password = request.form["password"]
 
+    check_pass = []
+    for i in password:
+      if i in ["`","~","!","@","#","$","%","^","&","*","(",")","[","-","_","=","+","]","{","}","\\","|",";",":","'","\"",",","<",".",">","/","?"]:
+        special_char = True
+      else:
+        special_char = False
+      if special_char:
+        if len(password) >= 7:
+          passworks = True
+        else:
+          passworks = False
+      else:
+        passworks = False
+      if passworks != True:
+        return render_template("invalidpass.html")
+    return render_template("loginoutput.html", username=username, password=password)
+
+
+@app.errorhandler(404)
+def not_found(e):
+  return render_template("404.html")
 
 app.run(host="0.0.0.0", port=8080)
