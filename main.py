cd091efddd717a66411d65c87aa2a84a83b8542f
kokomi simp :3 <beol0127@gmail.com>
2022-03-01 19:15:55 +0000

    chore: remove session + update gitignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request, flash, session, url_for, send_from_directory, redirect, jsonify
+from flask import Flask, render_template, request, flash, url_for, send_from_directory, redirect, jsonify
 from werkzeug.exceptions import abort
-from flask_session import Session
 from replit import db
 import sqlite3
 import markdown2
@@ -13,9 +12,6 @@ app = Flask(__name__,
             static_folder='static',
             template_folder='templates')
 app.config["SECRET_KEY"] = os.environ["key"]
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 
 def get_db_connection():
@@ -76,8 +72,6 @@ def loginrequest():
 
   if username in db["username"]:
     flash("Invalid username! It already exists! Try again in a few seconds!")
-  else:
-    session["user_exists"] = True
     db["username"].append(username)
   return redirect(url_for('index'))
 
